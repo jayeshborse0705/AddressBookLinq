0697fbe5c22982df37bf25fe2c211e49f65d929d
jayeshborse0705 <jayeshborse0705@gmail.com>
2022-06-22 20:09:16 +0530

    [jayesh] create address book

---
+++ AddressBookLinq/Program.cs
@@ -0,0 +1,14 @@
+﻿using AddressBookLinq;
+using System;
+namespace AddressBookLINQ
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            AddressBook addressBook = new AddressBook();
+            addressBook.CreateDataTable();
+
+        }
+    }
+}
